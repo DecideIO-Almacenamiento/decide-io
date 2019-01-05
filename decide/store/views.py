87609270bdcb6f29a9da52f7d42f5ebc9b1f0ccd
Pedro <pedpujorb@alum.us.es>
2019-01-05 10:37:09 +0100

    (REFACTOR) nuevo metodo save de voto
    Se ha extraido una funciÃn save del voto para comprobar si la API actual
    soporta la funcionalidad de edicion de voto
    
    /#3

--- decide/store/views.py
+++ decide/store/views.py
@@ -45,16 +45,7 @@ class StoreView(generics.ListAPIView):
         if not self.check_voter(token, uid, vid):
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
-        a = vote.get("a")
-        b = vote.get("b")
-
-        defs = { "a": a, "b": b }
-        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
-                                          defaults=defs)
-        v.a = a
-        v.b = b
-
-        v.save()
+        self.save_vote(vote, vid, uid)
 
         return  Response({})
 
@@ -86,3 +77,15 @@ class StoreView(generics.ListAPIView):
             return False
 
         return True
+
+    def save_vote(self, vote, vid, uid):
+        a = vote.get("a")
+        b = vote.get("b")
+
+        defs = { "a": a, "b": b }
+        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
+                                          defaults=defs)
+        v.a = a
+        v.b = b
+
+        v.save()
