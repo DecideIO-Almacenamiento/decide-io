e21a3f3f1030d77a3c136fcb7cd4f482221f72ac
jesecaguUs <jesescagu@alum.us.es>
2019-01-12 19:27:06 +0100

    (FEAT) Cambio script backups.py
    se ha añadido el metodo para averiguar la ruta absoluta del archivo
    backups.py

--- decide/store/backups.py
+++ decide/store/backups.py
@@ -8,9 +8,9 @@ database = 'decide'
 
 fileName = 'backup.sql' #nombre del fichero de backup y su extension.
 
-rutaBase = os.getcwd()  #Ruta absoluta donde se encuentra este archivo.
+rutaBase = os.path.abspath(os.path.dirname(__file__)) #Ruta absoluta donde se encuentra este archivo.  
 rutaBase = rutaBase.replace("\\","/")
-rutaBackups = '/store/backups/'+fileName
+rutaBackups = '/backups/'+fileName
 
 cmd1 = 'SET PGPASSWORD='+password   #Para que no pida confirmacion de la contraseña y se almacene en esa sesion de la consola.
 cmd2  = 'pg_dump -U '+user+' -f '+rutaBase+rutaBackups+' '+database     #Volcado de la base de datos a un archivo
